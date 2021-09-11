525c57aef4b7d7aa43cb3278a32d171d99ab9a3d
Mário Fernandes <mariobmf02@gmail.com>
2021-09-11 15:50:37 -0300

    feat: Initial Commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StatusBar, Text, View } from 'react-native';
+
+const App: React.FC = () => (
+  <View>
+    <StatusBar barStyle="dark-content" backgroundColor="#FFF" />
+    <Text>Initial Commit</Text>
+  </View>
+);
+
+export default App;
