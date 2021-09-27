efe8842bfdb806dac81ce8188033e7e144910d1d
Mário Fernandes <mariobmf02@gmail.com>
2021-09-27 20:07:57 -0300

    fix: configured splash screen and pdf screen

--- src/components/PdfViewer/index.tsx
+++ src/components/PdfViewer/index.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Container, PdfView } from './styles';
+import { Container, PageText, PdfView } from './styles';
 
 const PdfViewer: React.FC = () => {
+  const [page, setPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(0);
+
   const source = {
+    // uri: 'http://samples.leanpub.com/thereactnativebook-sample.pdf',
     uri: 'https://studiosol-a.akamaihd.net/gcs/cifraclub/contrib/partituras/bruno-e-marrone-boate-azul.pdf',
     cache: false,
   };
   return (
     <Container>
-      <PdfView source={source} enablePaging horizontal />
+      <PageText>
+        {page} / {totalPage}
+      </PageText>
+      <PdfView
+        source={source}
+        horizontal
+        enablePaging
+        spacing={0}
+        onLoadComplete={numberOfPages => {
+          setTotalPage(numberOfPages);
+        }}
+        onPageChanged={currentPage => {
+          setPage(currentPage);
+        }}
+      />
     </Container>
   );
 };
--- src/components/PdfViewer/styles.ts
+++ src/components/PdfViewer/styles.ts
@@ -3,9 +3,19 @@ import Pdf from 'react-native-pdf';
 
 export const Container = styled.View`
   flex: 1;
+  margin: 0 8px;
+`;
+
+export const PageText = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  color: #b50c09;
+  text-align: center;
+  margin-bottom: 8px;
 `;
 
 export const PdfView = styled(Pdf)`
   flex: 1;
   background: #fff;
+  /* margin: 8px; */
 `;
